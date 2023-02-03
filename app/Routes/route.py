62ada15c6a5f481dd8453171b319345e0ad17d1e
Rifai Martin <academyyyy12@gmail.com>
2023-02-03 13:16:08 +0700

    feat(create-card): create end point create one card

--- app/Routes/route.py
+++ app/Routes/route.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+from random import randrange
 from datetime import datetime
+
+from Models.card import Card
 
 router = APIRouter()
 
@@ -29,4 +32,16 @@ dataCards = [{
 @router.get("/cards")
 def get_cards():
     return {"data": dataCards}
+
+# Create one Card
+@router.post("/cards", status_code=status.HTTP_201_CREATED)
+def create_card(card: Card):
+    card_dict = card.dict()
+    card_dict['id'] = randrange(0, 100)
+    card_dict['created_at'] = newDate
+    card_dict['is_deleted'] = False
+    
+    # print(card)
+    dataCards.append(card_dict)
+    return {"data": card_dict}
 
